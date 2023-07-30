6a133327c9a4b183fb208ce745621ffdb2c3c647
Guilherme dos Santos <gs92dev@gmail.com>
2023-07-29 20:24:11 -0500

    finishes level 1 day 2

--- 2.js
+++ 2.js
@@ -1,35 +1,66 @@
 // 1. Declare a variable named challenge and assign it to an initial value **'30 Days Of JavaScript'**.
-let challenge = "30 Days Of JavaScript";
+    let challenge = "30 Days Of JavaScript";
 // 2. Print the string on the browser console using __console.log()__
-console.log(challenge);
+    console.log(challenge);
 // 3. Print the __length__ of the string on the browser console using _console.log()_
-console.log(challenge.length);
+    console.log(challenge.length);
 // 4. Change all the string characters to capital letters using __toUpperCase()__ method
-function print(str) {console.log(str);}
-print(challenge.toUpperCase());
+    function print(str) {
+        console.log(str);
+    }
+    print(challenge.toUpperCase());
 // 5. Change all the string characters to lowercase letters using __toLowerCase()__ method
-print(challenge.toLowerCase());
+    print(challenge.toLowerCase ());
 // 6. Cut (slice) out the first word of the string using __substr()__ or __substring()__ method
-print(challenge.substr(0,2));
+    print(challenge.substr(0, 2));
 // 7. Slice out the phrase *Days Of JavaScript* from *30 Days Of JavaScript*.
+    print(challenge.substring(2, challenge.length));
 // 8. Check if the string contains a word __Script__ using __includes()__ method
+    print(challenge.includes('Script')); //true
 // 9. Split the __string__ into an __array__ using __split()__ method
+    print(challenge.split(''));
 // 10. Split the string 30 Days Of JavaScript at the space using __split()__ method
+    print(challenge.split(' '));
 // 11. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' __split__ the string at the comma and change it to an array.
+    let bigTechs = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+    print(bigTechs.split(', '));
 // 12. Change 30 Days Of JavaScript to 30 Days Of Python using __replace()__ method.
+    print(challenge.replace('JavaScript', `Python`));
+    print(challenge);
 // 13. What is character at index 15 in '30 Days Of JavaScript' string? Use __charAt()__ method.
+    print(challenge.charAt(15)); //S
 // 14. What is the character code of J in '30 Days Of JavaScript' string using __charCodeAt()__
+    print(challenge.charCodeAt(11)); //
+    print(challenge.charAt(11));
 // 15. Use __indexOf__ to determine the position of the first occurrence of __a__ in 30 Days Of JavaScript
+    print(challenge.indexOf("a"));
 // 16. Use __lastIndexOf__ to determine the position of the last occurrence of __a__ in 30 Days Of JavaScript.
+    print(challenge.lastIndexOf("a"));
 // 17. Use __indexOf__ to find the position of the first occurrence of the word __because__ in the following sentence:__'You cannot end a sentence with because because because is a conjunction'__
+    let sentence= 'You cannot end a sentence with because because because is a conjunction';
+    print(sentence.indexOf("because"));
 // 18. Use __lastIndexOf__ to find the position of the last occurrence of the word __because__ in the following sentence:__'You cannot end a sentence with because because because is a conjunction'__
+    print(sentence.lastIndexOf("because"));
 // 19. Use __search__ to find the position of the first occurrence of the word __because__ in the following sentence:__'You cannot end a sentence with because because because is a conjunction'__
+    print(sentence.search("because"));
 // 20. Use __trim()__ to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+    let spaces = " 30 days of JavaScript "
+    print(spaces.trim());
 // 21. Use __startsWith()__ method with the string *30 Days Of JavaScript* and make the result true
+    print(challenge.startsWith("30")); //true
+    print(challenge.startsWith(30)); //true
+    
 // 22. Use __endsWith()__ method with the string *30 Days Of JavaScript* and make the result true
+    print(challenge.endsWith("t")); //true
+    print(challenge.endsWith("pt")); //true
+    print(challenge.endsWith(" pt")); //false
+    print(challenge.endsWith("JavaScript")); //true
 // 23. Use __match()__ method to find all the __a__’s in 30 Days Of JavaScript
+    print(challenge.match(/a/g)); 
 // 24. Use __concat()__ and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+    print("30 Days of ".concat("JavaScript"));  
 // 25. Use __repeat()__ method to print 30 Days Of JavaScript 2 times
+    print(challenge.repeat(2));
 
 // ### Exercise: Level 2
 
