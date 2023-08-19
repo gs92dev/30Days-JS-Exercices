2dbd26f0c81d18c184973e3bb5fc69f10d0d4773
Guilherme dos Santos <gs92dev@gmail.com>
2023-08-18 20:55:18 -0500

    Finished level 1 day 4

--- 4.js
+++ 4.js
@@ -3,6 +3,12 @@
 // ### Exercises: Level 1
 
 // 1. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to  wait for the number of years he needs to turn 18.
+// let age = prompt("Please enter your age");
+// if (age > 18) {
+//   alert("You can drive");
+// } else {
+//   alert("sorry you can't drive");
+// }
 
 //    ```sh
 //    Enter your age: 30
@@ -12,34 +18,37 @@
 //    You are left with 3 years to drive.
 //    ```
 
-// 1. Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
-let yourAge = prompt("Enter your age");
-let myAge = 30;
-if (myAge > yourAge) {
-  alert("I'm older than you");
-} else alert("You are older than me");
+// 2. Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+// let yourAge = prompt("Enter your age");
+// let myAge = 30;
+// if (myAge > yourAge) {
+//   alert("I'm older than you");
+// } else alert("You are older than me");
 
 //    ```sh
 //    Enter your age: 30
 //    You are 5 years older than me.
 //    ```
 
-// 1. If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+// 3. If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
 
 //     - using if else
 //     - ternary operator.
 
-//     ```js
-//       let a = 4
-//       let b = 3
+let a = 4;
+let b = 3;
+a > b ? console.log("a is greater") : console.log("b is greater");
 //     ```
 
 //     ```sh
 //       4 is greater than 3
 //     ```
 
-// 1. Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
-
+// 4. Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
+let number = prompt("please enter a number");
+number % 2 == 0
+  ? console.log(`${number} is an even number`)
+  : console.log(number + " is an odd number ");
 //     ```sh
 //     Enter a number: 2
 //     2 is an even number
