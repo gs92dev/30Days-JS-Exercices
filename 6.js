e1746213216394a7de3fd12d2539c2db70eb9a60
Guilherme dos Santos <gs92dev@gmail.com>
2023-09-02 12:49:21 -0500

    Staterted day 6

--- 6.js
+++ 6.js
@@ -2,37 +2,47 @@
 
 // ### Exercises: Level 1
 
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
 
-  const countries = [
-    'Albania',
-    'Bolivia',
-    'Canada',
-    'Denmark',
-    'Ethiopia',
-    'Finland',
-    'Germany',
-    'Hungary',
-    'Ireland',
-    'Japan',
-    'Kenya'
-  ];
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
 
-  const webTechs = [
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'React',
-    'Redux',
-    'Node',
-    'MongoDB'
-  ]
-
-  const mernStack = ['MongoDB', 'Express', 'React', 'Node']
-
+const mernStack = ["MongoDB", "Express", "React", "Node"];
 
 // 1. Iterate 0 to 10 using for loop, do the same using while and do while loop
-// 2. Iterate 10 to 0 using for loop, do the same using while and do while loop
-// 3. Iterate 0 to n using for loop
+// for (let i = 0; i < 11; i++) {
+//   console.log(i);
+// }
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+// let j= 0;
+// do{
+//   console.log(j);
+//   j++;
+// }while (j <= 10)
+
 // 4. Write a loop that makes the following pattern using console.log():
 
 //    ```js
@@ -47,47 +57,51 @@
 
 // 5. Use loop to print the following pattern:
 
-//    ```sh
-//    0 x 0 = 0
-//    1 x 1 = 1
-//    2 x 2 = 4
-//    3 x 3 = 9
-//    4 x 4 = 16
-//    5 x 5 = 25
-//    6 x 6 = 36
-//    7 x 7 = 49
-//    8 x 8 = 64
-//    9 x 9 = 81
-//    10 x 10 = 100
-//    ```
+for (let i = 0; i < 11; i++) {
+  console.log(`${i} x ${i} = ${i * i}`);
+}
 
 // 6. Using loop print the following pattern
-
-//    ```sh
-//     i    i^2   i^3
-//     0    0     0
-//     1    1     1
-//     2    4     8
-//     3    9     27
-//     4    16    64
-//     5    25    125
-//     6    36    216
-//     7    49    343
-//     8    64    512
-//     9    81    729
-//     10   100   1000
-//    ```
+for (let i = 0; i < 11; i++) {
+  console.log(`${i} x ${i * i} = ${i * i * i}`);
+}
 
 // 7. Use for loop to iterate from 0 to 100 and print only even numbers
+for (let index = 0; index <= 100; index++) {
+  if (index % 2 == 0) {
+    console.log(index);
+  }
+}
 // 8. Use for loop to iterate from 0 to 100 and print only odd numbers
+for (let index = 0; index <= 100; index++) {
+  if (index % 2 != 0) {
+    console.log(index);
+  }
+}
 // 9. Use for loop to iterate from 0 to 100 and print only prime numbers
 // 10. Use for loop to iterate from 0 to 100 and print the sum of all numbers.
 
 //     ```sh
 //     The sum of all numbers from 0 to 100 is 5050.
 //     ```
-
+let sum = 0;
+for (let index = 0; index <= 100; index++) {
+  sum = sum + index;
+}
+console.log(sum);
 // 11. Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+let odd = 0;
+let even = 0;
+for (let index = 0; index <= 100; index++) {
+  if (index % 2 != 0) {
+    odd = odd + index;
+  } else {
+    even = even + index;
+  }
+}
+console.log(even);
+
+console.log(odd);
 
 //     ```sh
 //     The sum of all evens from 0 to 100 is 2550. And the sum of all odds from 0 to 100 is 2500.
